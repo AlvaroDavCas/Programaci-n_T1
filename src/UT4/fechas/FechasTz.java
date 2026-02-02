3c5afe9f4a6d8ecb84d0c694dcdebe69491fb193
alvar <alvarodaca@outlook.es>
2026-02-02 23:27:02 +0100

    Ejercicios fechas

---
+++ src/UT4/fechas/FechasTz.java
@@ -0,0 +1,16 @@
+package UT4.fechas;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class FechasTz {
+    public static void main(String[] args) {
+
+        ZonedDateTime españa = ZonedDateTime.now(ZoneId.of("Europe/Madrid"));
+        ZonedDateTime usa = españa.withZoneSameInstant(ZoneId.of("America/New_York"));
+
+        System.out.println(españa);
+        System.out.println(usa);
+    }
+
+}
